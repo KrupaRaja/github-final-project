3dcee311b66ad87454fa203f56ff12e2a9488da3
KrupaRaja <36296535+KrupaRaja@users.noreply.github.com>
2023-09-27 11:57:15 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Krupa Raja (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Krupa Raja
    # Input:
    # p, principal amount
    # t, time period in years
